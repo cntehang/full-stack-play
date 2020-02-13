b42c928d99e907ef65008d4eda3530fcf78df770
yingliu <yingliu4203@gmail.com>
2020-02-13 12:46:46 -0800

    rename id and attribute names

--- app/assets/js/interaction/todo-demo.js
+++ app/assets/js/interaction/todo-demo.js
@@ -2,15 +2,15 @@ const TODO_ID_ATTR = 'data-todo-id'
 const selectorIds = `input[${TODO_ID_ATTR}]`
 const selectorChecked = `${selectorIds}:checked`
 
-const selected_form$ = $('#selected_form')
+const selectedForm$ = $('#selectedForm')
 
-const info_dialog$ = $('#info_dialog')
+const infoDialog$ = $('#infoDialog')
 
-
+// don't submit if nothing selected
 $('#showSelectedBtn').click(function (event) {
   const checkedItems$ = $(selectorChecked)
   if (checkedItems$.length < 1) {
-    info_dialog$.modal("show")
+    infoDialog$.modal("show")
     event.preventDefault()
   }
 })
@@ -19,26 +19,26 @@ $('#showSelectedBtn').click(function (event) {
 // have to append multiple inputs because
 // Play requires "[]"" as a postfix for an array field
 // and each value is separate
-selected_form$.submit(function () {
+selectedForm$.submit(function () {
   const checkedItems$ = $(selectorChecked)
   checkedItems$.each(function () {
     const id = $(this).attr(TODO_ID_ATTR)
     const newInput = $('<input>', {
       type: 'hidden',
-      name: 'ids[]', // must be the same as backedn
+      name: 'ids[]', // must be the same as backend
       value: id,
     });
-    selected_form$.append(newInput)
+    selectedForm$.append(newInput)
   })
 })
 
-$('#select_all_btn').click(function () {
+$('#selectAllBtn').click(function () {
   $(selectorIds).each(function () {
     $(this).prop('checked', true)
   })
 })
 
-$('#unselect_all_btn').click(function () {
+$('#unselectAllBtn').click(function () {
   $(selectorIds).each(function () {
     $(this).prop('checked', false)
   })
